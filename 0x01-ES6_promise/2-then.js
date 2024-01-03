534aa537f8f142a4c62782ae271b06bd05dbc665
Judhunja <judxhunja@gmail.com>
2024-01-03 18:28:56 +0300

    .then(doSomeThing), .catch(error)

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      const obj = {
+        status: 200,
+        body: 'success',
+      };
+      console.log('Got a response from the API');
+      return (obj);
+    })
+    .catch(() => Error());
+}
