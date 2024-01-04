7b6bd259271b0207481f3900c0ec9422efcad926
Judhunja <judxhunja@gmail.com>
2024-01-04 17:37:04 +0300

    added throwing typerrors for wrong type

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -12,6 +12,8 @@ export default class HolbertonCourse {
   set name(name) {
     if (typeof (name) === 'string') {
       this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
 
@@ -22,6 +24,8 @@ export default class HolbertonCourse {
   set length(length) {
     if (typeof (length) === 'number') {
       this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
     }
   }
 
@@ -32,6 +36,8 @@ export default class HolbertonCourse {
   set students(students) {
     if (Array.isArray(students) && students.every((i) => typeof i === 'string')) {
       this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
