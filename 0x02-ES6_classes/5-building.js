eb5e2c9ef3a5a67515dd33d5fd5427fb5b5ff062
Judhunja <judxhunja@gmail.com>
2024-01-04 21:29:27 +0300

    removing setter

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,18 +4,10 @@ export default class Building {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft === 'number') {
-      this._sqft = sqft;
-    } else {
-      throw new TypeError('Sqft must be a number');
-    }
   }
 }
