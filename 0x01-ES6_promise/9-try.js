7a86b6dc4cfe89fd5aca398fc7103f9375db270a
Judhunja <judxhunja@gmail.com>
2024-01-04 15:21:49 +0300

    cathching an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,10 @@
-export default function guardrail((mathFunction() => {
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const ret = mathFunction();
+    queue.push(ret, 'Guardrail was processed');
+  } catch (e) {
+    queue.push(`${e}`, 'Guardrail was processed');
+  }
+  return queue;
+}
