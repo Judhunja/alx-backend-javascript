9d72571ad3ccfe3c7067551c98a4837fafde6f33
Judhunja <judxhunja@gmail.com>
2024-01-04 10:55:34 +0300

    returning a Promise with resolved promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const namePromise = createUser();
-
-  Promise.all([photoPromise, namePromise]).then((vals) => {
-    const [photo, name] = vals;
-    console.log(`${photo.body} ${name.firstName} ${name.lastName}`);
-  }).catch((error) => {
-    console.error('Signup system offline', error);
-  });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((vals) => {
+      const [photo, name] = vals;
+      console.log(`${photo.body} ${name.firstName} ${name.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
