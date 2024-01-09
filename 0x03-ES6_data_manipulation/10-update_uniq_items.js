f51bbc3e73861d966bf41893feca40e61220650d
Judhunja <judxhunja@gmail.com>
2024-01-09 19:22:01 +0300

    updating specific values of a Map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((quantity, name) => {
+    if (quantity === 1) {
+      map.set(name, 100);
+    }
+  });
+  return map;
+}
