97a96ba45e7671176672c7338b131da74baba2b1
Judhunja <judxhunja@gmail.com>
2024-01-09 16:03:38 +0300

    added error checks for 8-clean_set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   let newstr = '';
   for (const elem of set) {
-    if (elem.startsWith(startString) && startString !== '') {
+    if (elem.startsWith(startString) && set instanceof Set && typeof startString === 'string' && elem !== undefined && elem !== null && startString !== '' && startString !== null && startString !== undefined) {
       if (newstr !== '') {
         newstr += '-';
       }
