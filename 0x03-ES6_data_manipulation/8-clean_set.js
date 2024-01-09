01f2749c56aa36a4f3a20889c95d12b4e0ce8bc0
Judhunja <judxhunja@gmail.com>
2024-01-09 18:06:16 +0300

    return null if any elem of set is not a string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
     return newstr;
   }
   for (const elem of set) {
-    if (!elem || typeof elem !== 'string' || elem.length < startString.length) return '';
+    if (!elem || typeof elem !== 'string' || elem.length < startString.length) return null;
     if (elem.startsWith(startString)) {
       if (newstr !== '') {
         newstr += '-';
