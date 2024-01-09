3354afd959ead73560e36dc12e56f7d4399303a0
Judhunja <judxhunja@gmail.com>
2024-01-09 13:38:11 +0300

    error check if index is greater than array length

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    const arrayBuffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(arrayBuffer);
-
-    int8Array[position] = value;
-
-    return new DataView(arrayBuffer);
-  } catch (error) {
+  if (position > length) {
     throw new Error('Position outside range');
   }
+  const arrayBuffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(arrayBuffer);
+
+  int8Array[position] = value;
+
+  return new DataView(arrayBuffer);
 }
