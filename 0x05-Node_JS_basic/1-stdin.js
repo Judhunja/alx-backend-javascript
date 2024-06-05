58365f02971432ada8f81a38ce0ad5a9844fbc7a
Judhunja <judxhunja@gmail.com>
2024-06-05 21:42:58 +0300

    exporting function if not called on the command line, running it as main otherwise

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,22 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf-8');
+function writeStdout() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.setEncoding('utf-8');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
-});
-
-if (!process.stdin.isTTY) {
-  process.on('exit', () => {
-    console.log('This important software is now closing\n');
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit();
   });
+
+  if (!process.stdin.isTTY) {
+    process.on('exit', () => {
+      console.log('This important software is now closing\n');
+    });
+  }
+}
+
+if (require.main === module) {
+  writeStdout();
+} else {
+  module.exports = writeStdout;
 }
