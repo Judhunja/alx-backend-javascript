9427d5b5f39daa28df91b764a3127d6cc9d2e3ce
Judhunja <judxhunja@gmail.com>
2024-06-08 15:46:58 +0300

    remove toString conversion before trimming the input string

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ function writeStdout() {
   process.stdin.setEncoding('utf-8');
 
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
+    const name = data.trim();
     process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
   });
